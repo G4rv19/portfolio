3c20fe2048c8cc638537322903658a25628f013f
G4rv19 <garvsidana29@gmail.com>
2024-01-26 00:37:34 +1000

    added some links, changed router link, add pics and css file

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("home.html")
+@app.route('/')
+def default_redirect():
+    return redirect(url_for('homepage'))
+
+@app.route('/home')
+def homepage():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
