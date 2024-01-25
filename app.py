d12eadab06091af8b6c440d39a97f3a6f3a00fe1
G4rv19 <garvsidana29@gmail.com>
2024-01-25 21:05:27 +1000

    added changes to files

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
