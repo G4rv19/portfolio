6549030a667bcf62deac3ddd332dc2cb48365a84
G4rv19 <123931565+G4rv19@users.noreply.github.com>
2024-02-04 02:39:17 +1000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,12 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def default_redirect():
-    return redirect(url_for('homepage'))
-
-@app.route('/home')
 def homepage():
     return render_template('home.html')
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
