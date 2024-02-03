9e4f4afdde5e2c87fa0e892cfaf37470e0786242
G4rv19 <garvsidana29@gmail.com>
2024-02-04 01:46:06 +1000

    ....

--- vercel_wrapper.py
+++ vercel_wrapper.py
@@ -5,7 +5,7 @@ with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
 from flask import Flask, render_template
-from your_project.app.your_flask_app_file import create_app
+from app import create_app
 
 app = create_app()
 
