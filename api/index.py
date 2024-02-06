be00282928458c0c2be24fb5222ec9e718c5a9b1
G4rv19 <garvsidana29@gmail.com>
2024-02-06 13:08:33 +1000

    reverse the changes

--- api/index.py
+++ api/index.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
-from js import inject
-
 
 app = Flask(__name__)
-inject()
+
 @app.route('/')
 def homepage():
     return render_template('home.html')
