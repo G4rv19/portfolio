87ef2258c652eccea2f4ef186ae59b2bb6d7b3bd
G4rv19 <garvsidana29@gmail.com>
2025-03-13 00:27:35 +1000

    fixed the images path

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__, static_url_path='/public', static_folder='../public')
+app = Flask(__name__, static_url_path='/public', static_folder='public')
 
 @app.route('/')
 def homepage():
